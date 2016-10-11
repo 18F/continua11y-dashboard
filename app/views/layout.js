8b7927d445c060e94fe09e04564ded304b087bd2
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-10-11 09:21:12 -0700

    Programming css like its 1995

--- app/views/layout.js
+++ app/views/layout.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   site_title: 'Continua11y - A continuous integration dashboard for web accessibility',
+  site_description_small: 'A continuous integration dashboard for web accessibility',
   site_description: 'Continua11y allows developers to get reports on their site\'s accessibility while they\'re building'
 };
