0f16c16b084e1c67d50ae9d6e4fa85ae7a942bf0
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-10-14 09:58:48 -0700

    fail: removes logging line that showed up in tests :(

--- app/controllers/error.js
+++ app/controllers/error.js
@@ -4,7 +4,6 @@ const MustacheRenderer = require('../mustache-renderer');
 
 module.exports = function notFound(error, req, res, next) {
   // TODO: better logging
-  console.log(error);
   res.status(500);
   let renderer = new MustacheRenderer('500');
   res.send(renderer.render());
